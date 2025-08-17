c281c512db21d9ca54b4953f2d3518af0614395e
mostafa <mostafa.zizp@gmail.com>
2025-08-17 22:23:23 +0300

    create .gitignore file

--- config/compression_script.sh
+++ config/compression_script.sh
@@ -1,6 +1,6 @@
 create_backup() {
     local source_path="$1"
-    local backup_path="${HOME}/autoback/backup/$2"
+    local backup_name="$2"
     local exclude_pattern="_info.txt"
     local use_encryption="$3"
     local password="$4"
@@ -8,32 +8,33 @@ create_backup() {
     # Base tar command
     local tar_cmd="tar -czvf"
     
-    if [[ "$use_encryption" == "yes" ]]; then
+    if [[ "$use_encryption" == 1 ]]; then
         echo "Creating encrypted backup..."
+        local backup_path="${HOME}/autoback/backup/${backup_name}.gpg"
         
         # Using GPG for encryption
         if command -v gpg >/dev/null 2>&1; then
-            if [[  $password -eq 1 ]]; then
+            if [[ -n "$password" ]]; then
                 tar -czvf - --exclude="$exclude_pattern" "$source_path" | \
-                gpg --batch --yes --force --passphrase "$password" \
+                gpg --batch --yes --passphrase "$password" \
                     --symmetric --cipher-algo AES256 \
                     --compress-algo 1 --s2k-mode 3 \
                     --s2k-digest-algo SHA512 \
                     --s2k-count 65011712 \
                     --force-mdc \
-                    --output "${backup_path}.gpg"
+                    --output "$backup_path"
             else
                 tar -czvf - --exclude="$exclude_pattern" "$source_path" | \
-                gpg --force --symmetric --cipher-algo AES256 \
+                gpg --yes --symmetric --cipher-algo AES256 \
                     --compress-algo 1 --s2k-mode 3 \
                     --s2k-digest-algo SHA512 \
                     --s2k-count 65011712 \
                     --force-mdc \
-                    --output "${backup_path}.gpg"
+                    --output "$backup_path"
             fi
             
             if [[ $? -eq 0 ]]; then
-                echo "✅ Encrypted backup created: ${backup_path}.gpg"
+                echo "✅ Encrypted backup created: $backup_path"
             else
                 echo "❌ Failed to create encrypted backup"
                 return 1
@@ -44,6 +45,8 @@ create_backup() {
         fi
     else
         echo "Creating unencrypted backup..."
+        local backup_path="${HOME}/autoback/backup/${backup_name}.tar.gz"
+        
         tar -czvf "$backup_path" --exclude="$exclude_pattern" "$source_path"
         
         if [[ $? -eq 0 ]]; then
